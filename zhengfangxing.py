3a0b43dbff9faa292fb85dcff766e160ca2ffddb
gchzhyys <62194614+gchzhyys@users.noreply.github.com>
2020-05-06 21:49:37 +0800

    修改red为字符串

--- zhengfangxing.py
+++ zhengfangxing.py
@@ -1,7 +1,7 @@
 def fangxing(a,n,ps,pc):
     import turtle
     turtle.pensize(ps)
-    turtle.pencolor('pc')
+    turtle.pencolor(pc)
     for i in range(n-1):
         b = 1
         le = a + b * (i * 10)
@@ -10,5 +10,5 @@ def fangxing(a,n,ps,pc):
         turtle.fd(le)
         turtle.left(90)
 
-fangxing(20,50,5,red)
+fangxing(20,50,5,"red")
 done()
